4feb656d371491b9c7def3880d16d1c31cd30c38
lifaet <lifaethossain@gmail.com>
2024-01-12 16:12:22 +0600

    Update source.ino

--- source/source.ino
+++ source/source.ino
@@ -12,7 +12,6 @@
 #include <LiquidCrystal_I2C.h>
 #define greenLedPin 8
 #define redLedPin 9
-#define buzzerPin 10
 #define triggerPin 11
 #define echoPin 12
 #define maxDistance 50
@@ -22,7 +21,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 int smellReading = 0, distanceThreshold, gasThreshold;
 
 void setup() {
-  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
   pinMode(redLedPin, OUTPUT);
   pinMode(greenLedPin, OUTPUT);
   pinMode(gasPin, INPUT);
@@ -31,11 +30,13 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Calibrating...");
+  Serial.println("Calibrating...");
   delay(2000);
   distanceThreshold = (sonar.ping() / US_ROUNDTRIP_CM) - 2;
   gasThreshold = analogRead(gasPin) + 5;
   lcd.clear();
   lcd.print("Calibrated...");
+  Serial.println("Calibrated...");
   delay(500);
   lcd.clear();
 }
@@ -56,59 +57,63 @@ void foodAnalyze() {
   delay(500);
   lcd.setCursor(0, 1);
   lcd.print("  Analyzing... ");
+  Serial.println("Food Detected, Analyzing...");
   delay(10000);
   smellReading = analogRead(gasPin);
   if (smellReading < gasThreshold) {
     goodFood();
+    foodMonitor();
   } else {
     badFood();
+    foodMonitor();
   }
 }
 
 void goodFood() {
   digitalWrite(greenLedPin, HIGH);
   digitalWrite(redLedPin, LOW);
-  digitalWrite(buzzerPin, LOW);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Methane Rate:" + String(smellReading));
   lcd.setCursor(0, 1);
   lcd.print("  Food is Good  ");
-  foodMonitor();
+  Serial.println("Methane Rate:" + String(smellReading) + ", Food is Good");
 }
 
 void badFood() {
   digitalWrite(greenLedPin, LOW);
   digitalWrite(redLedPin, HIGH);
-  digitalWrite(buzzerPin, HIGH);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Methane Rate:" + String(smellReading));
   lcd.setCursor(0, 1);
   lcd.print(" Food  Spoiled ");
-  foodMonitor();
+  Serial.println("Methane Rate:" + String(smellReading) + ", Food is Spoiled");
 }
 
 void noFood() {
   digitalWrite(redLedPin, LOW);
   digitalWrite(greenLedPin, LOW);
-  digitalWrite(buzzerPin, LOW);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("   Box Empty   ");
   lcd.setCursor(0, 1);
   lcd.print("  Insert Food  ");
+  Serial.println("Box Empty, Insert Food");
+  delay(500);
+  digitalWrite(greenLedPin, HIGH);
+  delay(500);
 }
 
 void foodMonitor() {
-  while ((sonar.ping() / US_ROUNDTRIP_CM) < distanceThreshold) {
+  while ((sonar.ping() / US_ROUNDTRIP_CM) <= distanceThreshold) {
     delay(1000);
   }
-  digitalWrite(buzzerPin, LOW);
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("  Food Removed  ");
   lcd.setCursor(0, 1);
   lcd.print(" Refreshing... ");
+  Serial.println("Food Removed, Refreshing...");
   delay(5000);
 }
